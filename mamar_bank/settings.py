1e744c528b4defe66bb2ffc4169dde7f46fff4bf
mdshakibalrayhan <rayhanshakib14@gmail.com>
2025-01-19 01:37:22 +0600

    host added

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -55,7 +55,7 @@ SECRET_KEY = 'django-insecure-mes$n&n65xj%l60*%l1bo#g$c0s7j9c$jck6nuuq-=b@zbr_5u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
